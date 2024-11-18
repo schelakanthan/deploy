64b9cad7f61e35c4cdb9e8ff465afcb220828656
zujanshan <77782675+schelakanthan@users.noreply.github.com>
2024-11-18 20:34:54 +0530

    chore :wegtail

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-e*1=^+840r8f@^xe#kh-o9oz=b&933p^6supe$yi#7kqvtc$^*"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
